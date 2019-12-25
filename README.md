6f2e839c962d71dede354f467b96905597d586f4
eduqg <eduardoqgomes@gmail.com>
2019-12-25 20:06:47 -0300

    Initial get one, get all, post and delete with parse

---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+Instale as dependências
+
+```console
+yarn
+```
+
+Configure as variáveis de ambiente no arquivo .env para acessar o backend da aplicação de acordo com o arquivo de exemplo.
+
+```console
+cp .env-example .env
+```
+
+Execute o projeto.
+```console
+yarn start
+```
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,38 +1,87 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-console */
+import React from 'react';
 
-import axios from 'axios';
+import Parse from 'parse';
 import { Container } from './styles';
 
 // import api from '../../services/api';
 
 export default function List() {
-  useEffect(() => {
-    console.log('Iniciou');
-    axios
-      .post(
-        'http://localhost:1337/parse/functions/hello',
-        {
-          name: 'Professor João',
-          parts: '',
-        },
-        {
-          headers: {
-            'X-Parse-Application-Id': 'myAppId',
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error.response);
-      });
-  }, []); // eslint-disable-line
+  Parse.initialize(
+    process.env.REACT_APP_APP_ID,
+    process.env.REACT_APP_JAVASCRIPT_KEY,
+    process.env.REACT_APP_MASTER_KEY
+  );
+
+  Parse.serverURL = 'https://server-teachers-list.herokuapp.com/parse';
+
+  async function handleSend() {
+    try {
+      console.log('Enviou');
+      const Post = Parse.Object.extend('Post');
+      const post = new Post();
+      const data = {
+        body: 'Hello my name is Eduardo',
+        tags: ['welcome', 'first post'],
+        numComments: 2,
+      };
+
+      const response = await post.save(data);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleGetOne(itemId) {
+    console.log('Get');
+
+    try {
+      const query = new Parse.Query('Post'); // eslint-disable-line
+      const response = await query.get(itemId);
+      console.log(response.attributes);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleGet() {
+    console.log('Get');
+
+    try {
+      const query = new Parse.Query('Post'); // eslint-disable-line
+
+      const response = await query.find();
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleDelete(itemId) {
+    console.log('Delete');
+
+    try {
+      const query = new Parse.Query('Post'); // eslint-disable-line
+      const object = await query.get(itemId);
+      console.log(object.attributes);
+
+      const response = await object.destroy();
+
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <Container>
       <h1>List</h1>
+      <button onClick={handleSend}>Enviar!</button>
+      <button onClick={() => handleGetOne('1elK2SAFjC')}>Get One!</button>
+      <button onClick={() => handleGet()}>Get All!</button>
+      <button onClick={() => handleDelete('vH2auypHT7')}>Delete!</button>
     </Container>
   );
 }
